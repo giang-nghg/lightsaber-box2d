ad06b21b9c0076cae2aca1729c568ad9453ae50f
Lion User <User@Lions-Mac.local>
2012-10-15 18:35:46 +0700

    Initial Commit

---
+++ lightsaber_box2d/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  lightsaber_box2d
+//
+//  Created by Lion User on 10/15/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ lightsaber_box2d/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lightsaber_box2d
+//
+//  Created by Lion User on 10/15/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
